a64e0e744dbdebb73f2c1785ca7a25120103af62
Anton Richter <ar140@hdm-stuttgart.de>
2021-05-16 16:01:02 +0200

    updated init

--- __init__.py
+++ __init__.py
@@ -1,15 +1,17 @@
 from mycroft import MycroftSkill, intent_file_handler
-import GetDataFromNextcloud as data
+import GetDataFromNextcloud
 
 class CalendarManager(MycroftSkill):
    
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.next_appointment = GetDataFromNextcloud.GetDataFromNextcloud().whats_my_next_appointment()
+        
 
     @intent_file_handler('manager.calendar.intent')
     def handle_manager_calendar(self, message):
-        self.speak_dialog('manager.calendar')
-        print(data.GetDataFromNextcloud.whats_my_next_appointment())
+        self.speak_dialog('manager.calendar', {'date':self.next_appointment.DTSTART}, {'title':self.next_appointment.SUMMARY})
+        print(self.next_appointment)
 
 
 def create_skill():
