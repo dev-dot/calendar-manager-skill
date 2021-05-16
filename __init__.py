748f18e7461e2d9c52c625a9594bdeae75b82b37
Anton Richter <ar140@hdm-stuttgart.de>
2021-05-16 18:44:58 +0200

    next ***** try

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ class CalendarManager(MycroftSkill):
         self.username = 'ar140@hdm-stuttgart.de'
         self.password = 'Mycroftgruppe5'
         self.client = caldav.DAVClient(url=self.caldav_url, username=self.username, password=self.password)
-
+        self.the_same_calendar = self.client.calendar(url="https://si-nextcloud.social-robot.info/remote.php/dav/calendars/ar140%40hdm-stuttgart.de/personal/")
 
     def get_calendars(self):
         principal = self.client.principal()
@@ -30,7 +30,8 @@ class CalendarManager(MycroftSkill):
 
     @intent_file_handler('ask.next.appointment.intent')
     def handle_manager_calendar(self, message):
-        next_appointment = self.whats_my_next_appointment()
+        next_appointment = self.whats_my_next_appointment(self.get_calendars()[0])
+        print(self.get_calendars()[0])
         self.speak_dialog('next.appointment', {'title':next_appointment["SUMMARY"]})           #{'date':self.next_appointment.DTSTART}, {'title':self.next_appointment.SUMMARY
 
 def create_skill():
