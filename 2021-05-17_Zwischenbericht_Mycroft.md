c9dc4488cb63b08a501caa77e05a550fa68842c5
Artur Pauss <ap096@hdm-stuttgart.de>
2021-05-17 22:05:06 +0200

    show appointment title
    Co-authored-by: dev-dot <dev-dot@users.noreply.github.com>

--- 2021-05-17_Zwischenbericht_Mycroft.md
+++ 2021-05-17_Zwischenbericht_Mycroft.md
@@ -5,3 +5,15 @@ Auf unserem Pi:
 * Pip3 upgraden und danach installierten wir msk mit dem Befehl pip3 install msk
 * Skill erstellt mit Hilfe des Templates.
 * Es war nicht Möglich den Skill auf das persönliche GitHub zu pushen, deshalb haben wir uns vom Template ein Repro erstellen lassen.
+
+## Weitere Erfahrungen 
+* Nextcloud Anbindung 
+    * Anhand der verlinkten Caldav Dokumentation 
+* Neues Repo mit Becker-Asano 
+* Probleme mit Caldav installation auf dem RasperryPi
+    * ´sudo apt-get install libxml2-dev libxslt-dev python-dev´ wird benötigt für die Installion von libxml2 
+    * Danach konnte ´pip3 install caldav´ ausgeführt werden.
+* Danach konnte der Skill ohne Fehler (Modul import error) geladen werden. 
+* Der Title vom Termin kann aus dem Terminkalender abegrufen und von Mycroft ausgegeben werden.
+
+
--- __init__.py
+++ __init__.py
@@ -6,8 +6,12 @@ from datetime import datetime
 import caldav
 from caldav.objects import Calendar
 import regex
+import icalendar 
+import pytz 
 
 class CalendarManager(MycroftSkill):
+    
+
 
     def __init__(self):
         MycroftSkill.__init__(self)
@@ -16,24 +20,65 @@ class CalendarManager(MycroftSkill):
         self.password = 'Mycroftgruppe5'
         self.client = caldav.DAVClient(url=self.caldav_url, username=self.username, password=self.password)
         self.the_same_calendar = self.client.calendar(url="https://si-nextcloud.social-robot.info/remote.php/dav/calendars/ar140%40hdm-stuttgart.de/personal/")
+        
 
     def get_calendars(self):
         principal = self.client.principal()
         calendars = principal.calendars()
-
+        
         return calendars
 
     def whats_my_next_appointment(self, calendar: Calendar):
         all_events = calendar.events()
-        return all_events[0].data
+        parse_next_event = self.parse_ics_events(all_events)
+        next_event_details = self.get_event_details(parse_next_event[0]) #ToDO change names
+        return next_event_details
 
+
+    def get_event_details(self, event):
+        
+        start = None
+        if "DTSTART" in event.keys():
+            start = event["DTSTART"].dt
+            if not isinstance(start, datetime):
+                start = datetime.combine(start, datetime.min.time())
+                start = start.replace(tzinfo=pytz.Utc)
+            else:
+                start = start.astimezone(self.local_timezone)
+        end = None
+        if "DTEND" in event.keys():
+            end = event["DTEND"].dt
+            if not isinstance(end, datetime):
+                end = datetime.combine(end, datetime.min.time())
+                end = end.replace(tzinfo=pytz.Utc)
+            else:
+                end = end.astimezone(self.local_timezone)
+        title = "untitled event"
+        if "SUMMARY" in event.keys():
+            title = str(event["SUMMARY"])
+
+        return {"title": title, "starttime": start, "endtime": end}
+
+    def parse_ics_events(self, events):
+        
+        parsed_events = []
+        for event in events:
+            cal = icalendar.Calendar.from_ical(event.data, True)
+            url = event.url
+            for vevent in cal[0].walk("vevent"):
+                event_details = self.get_event_details(vevent)
+                event_details["event_url"] = url
+                parsed_events.append(event_details)
+        return parsed_events
 
 
     @intent_file_handler('ask.next.appointment.intent')
     def handle_manager_calendar(self, message):
         next_appointment = self.whats_my_next_appointment(self.the_same_calendar)
-        title = ' '.join(regex.search("SUMMARY:.*?:", next_appointment, regex.DOTALL).group().replace(':', ' ').split()[1:-1])
-        self.speak_dialog('next.appointment', {'title':title})
+        #title = ' '.join(regex.search("SUMMARY:.*?:", next_appointment, regex.DOTALL).group().replace(':', ' ').split()[1:-1])
+
+
+        self.speak_dialog('next.appointment', {'title': next_appointment['SUMMARY']})
 
 def create_skill():
     return CalendarManager()
