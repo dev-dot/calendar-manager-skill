ad1b81496252d4f01e3b6d2621481b5dfe534317
Artur Pauss <ap096@hdm-stuttgart.de>
2021-05-28 15:20:15 +0200

    fix

--- __init__.py
+++ __init__.py
@@ -34,9 +34,9 @@ class CalendarManager(MycroftSkill):
     def whats_my_next_appointment(self, calendar: Calendar):
         currentDate = datetime.today()
 
-        # all_future_events = calendar.date_search(start=datetime(currentDate.year,currentDate.month,currentDate.day),end=datetime(currentDate.year+1,currentDate.month,currentDate.day),expand=True)
-        # parse_next_event = self.parse_ics_events(all_events)
-        all_events = self.get_all_events()
+        all_future_events = calendar.date_search(start=datetime(currentDate.year,currentDate.month,currentDate.day),end=datetime(currentDate.year+1,currentDate.month,currentDate.day),expand=True)
+        parse_next_event = self.parse_ics_events(all_future_events)
+        all_events = self.get_all_events(calendar)
         # print(parse_next_event[0])
         self.get_event_data_string(all_events[0])
         return parse_next_event[0]
