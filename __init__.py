97ac04eb982b81e25f36d26bafa9ad322bbe0c79
Artur Pauss <ap096@hdm-stuttgart.de>
2021-06-14 17:49:53 +0200

    ADD: Zwischenbericht | Timezone | Dialog change | Inputvalidation

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ import pytz
 
 Utc = pytz.UTC
 class CalendarManager(MycroftSkill):
-    
+
 
 
     def __init__(self):
@@ -26,7 +26,7 @@ class CalendarManager(MycroftSkill):
         self.password = self.settings.get('password')
         self.client = caldav.DAVClient(url=self.caldav_url, username=self.username, password=self.password)
         self.the_same_calendar = self.client.calendar(url = self.get_calendars()[0].url)
-        
+
 
     def get_calendars(self):
         principal = self.client.principal()
@@ -34,21 +34,7 @@ class CalendarManager(MycroftSkill):
         self.log.info(calendars)
         return calendars
 
-    #def whats_my_next_appointment(self, calendar: Calendar):
-      #  currentDate = datetime.today()
 
-    #    all_future_events = calendar.date_search(start=datetime(currentDate.year,currentDate.month,currentDate.day),end=datetime(currentDate.year+1,currentDate.month,currentDate.day),expand=True)
-     #   parse_next_event = self.parse_ics_events(all_future_events)
-    #    all_events = self.get_all_events(calendar, datetime.now())  
-        # print(parse_next_event[0])
-      #  print(all_events[0].data)
-       # return parse_next_event[0]
-
-    #def filter_events(self, events):
-
-    #    for event in events:
-      #      if "DTSTART":
-          
 
     def get_event_data_string(self, event):
         starttime = event.instance.vevent.dtstart.value
@@ -56,14 +42,14 @@ class CalendarManager(MycroftSkill):
 
 
     def get_all_events(self, calendar: Calendar, start: datetime = None, end: datetime = None):
-        
+
         all_events = []
 
         if start is None:
             return calendar.events()
         else:
             event_date = calendar.date_search(start=start, end=end)
-            
+
 
             for event in event_date:
                 event_start = event.instance.vevent.dtstart.value
@@ -75,13 +61,13 @@ class CalendarManager(MycroftSkill):
                 if event.instance.vevent.dtstart.value.astimezone() >= start.astimezone():
                     all_events.append(event)
             if end is not None:
-                all_events = [i for i in all_events if 
-                 i.instance.vevent.dtstart.value.astimezone() <= end.astimezone()] 
-            all_events.sort(key=lambda event: event.instance.vevent.dtstart.value.astimezone())     
+                all_events = [i for i in all_events if
+                 i.instance.vevent.dtstart.value.astimezone() <= end.astimezone()]
+            all_events.sort(key=lambda event: event.instance.vevent.dtstart.value.astimezone())
             return all_events
 
 
-        
+
 
 
     def get_event_details(self, event):
@@ -89,15 +75,9 @@ class CalendarManager(MycroftSkill):
         if "SUMMARY" in event.keys():
             title = str(event["SUMMARY"])
 
-       # event_start = "not choosed"    
-       # if "DTSTART" in event.key():
-       #     event_start = str(event["DTSTART"])
+     
 
-        #event_end = None     
-        #if "DTEND" in event.keys():
-        #    event_end = str(event["DTEND"]) '
-
-        return {"title": title} 
+        return {"title": title}
 
     def parse_ics_events(self, events):
         
@@ -113,7 +93,7 @@ class CalendarManager(MycroftSkill):
 
 
     def date_to_string(self, vevent_date: datetime, with_time: bool =True):
-   
+
         date_string = f"{vevent_date.strftime('%B')} {vevent_date.strftime('%d')}, {vevent_date.strftime('%Y')}"
         if with_time:
             date_string = date_string + f" at {vevent_date.strftime('%H:%M')}"
@@ -134,13 +114,13 @@ class CalendarManager(MycroftSkill):
                 'saturday'  : 5,
                 'sunday'    : 6
             }
-        return switcher.get(i,"Invalid day of week")
+        return switcher.get(i,-1)
 
     def search_date_from_weekday(self, weekday_int):
         today = date.today()
         next_date = today + relativedelta.relativedelta(weekday= weekday_int)
         return next_date
-    
+
     def get_ordinal_number(self,i):
         switcher={
             1: 'first',
@@ -205,50 +185,52 @@ class CalendarManager(MycroftSkill):
 
         weekday = message.data['weekday']
 
-        event_date = self.search_date_from_weekday(self.parse_weekday(weekday))
+        weekday_as_int = self.parse_weekday(weekday)
+        if weekday_as_int != -1:
+            event_date = self.search_date_from_weekday(weekday_as_int)
 
-        if (self.parse_weekday(weekday) == event_date.today().weekday()):
-            event_date = event_date + timedelta(days=7)
-            print(self.parse_weekday(weekday))
-            print(event_date.today().weekday())
+            if (self.parse_weekday(weekday) == event_date.today().weekday()):
+                event_date = event_date + timedelta(days=7)
+                print(self.parse_weekday(weekday))
+                print(event_date.today().weekday())
+            else:
+                event_date = event_date + timedelta(days=0)
+                print(self.parse_weekday(weekday))
+                print(event_date.today().weekday())
+
+            event_date_string = f"{self.get_ordinal_number(event_date.day)} of {event_date.strftime('%B')}"
+            start_search = datetime.combine(event_date,datetime.min.time().astimezone())
+            date_end = date(event_date.year,event_date.month,event_date.day+1)
+            end_search = datetime.combine(date_end, datetime.min.time().astimezone())
+
+            calendar = self.get_calendars()[0]
+            events = self.get_all_events(calendar= calendar, start= start_search.astimezone(), end= end_search.astimezone())
+            event_len = len(events)
+
+            if (len(events)==0):
+                self.speak_dialog('no.appointments.weekday', {'weekday':weekday, 'date':event_date_string})
+            elif(len(events)>=1):
+                self.speak_dialog('yes.appointments.weekday', {'number': event_len,'weekday':weekday, 'date':event_date_string})
+                for event in events:
+                    next_event = event.instance.vevent
+                    start = self.get_time_string(next_event.dtstart.value) #TODO: add Duration
+                    end = self.get_time_string(next_event.dtend.value)
+                    summary = next_event.summary.value
+
+                    self.log.info("Appointments found: %s",event_len)
+
+                    self.speak_dialog('yes.appointment.weekday.first', {'title': summary, 'start': start, 'end':end})
         else:
-            event_date = event_date + timedelta(days=0)
-            print(self.parse_weekday(weekday))
-            print(event_date.today().weekday())
-
-        event_date_string = f"{self.get_ordinal_number(event_date.day)} of {event_date.strftime('%B')}"
-        start_search = datetime.combine(event_date,datetime.min.time())
-        date_end = date(event_date.year,event_date.month,event_date.day+1)
-        end_search = datetime.combine(date_end, datetime.min.time())
-  
-        calendar = self.get_calendars()[0]
-        events = self.get_all_events(calendar= calendar, start= start_search, end= end_search)
-        event_len = len(events)
-
-        if (len(events)==0):
-            self.speak_dialog('no.appointments.weekday', {'weekday':weekday, 'date':event_date_string})
-        elif(len(events)>=1):
-            self.speak_dialog('yes.appointments.weekday', {'number': event_len,'weekday':weekday, 'date':event_date_string})
-            for event in events:
-                next_event = event.instance.vevent
-                start = self.get_time_string(next_event.dtstart.value) #TODO: add Duration
-                end = self.get_time_string(next_event.dtend.value)
-                summary = next_event.summary.value
-                
-                self.log.info("Appointments found: %s",event_len)
-                
-                self.speak_dialog('yes.appointment.weekday.first', {'title': summary, 'start': start, 'end':end})
-
-               
+            self.speak("This is not a weekday. Please rephrase your question.")
 
        # self.speak(weekday)
         self.log.info("Start date: %s", start_search)
         self.log.info("End Date: %s", end_search)
 
-    
-        #TODO: Timezone 
-        #TODO: Specific Date 
-        #TODO: Bewusste Anzahl an Terminenen ausrufen 
+
+        #TODO: Timezone
+        #TODO: Specific Date
+        #TODO: Bewusste Anzahl an Terminenen ausrufen
         #TODO: Bonusaufgaben
 
 def create_skill():
