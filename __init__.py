9acc6b4d4ed643eb56b5f3cab5fc922c22eab119
Artur Pauss <ap096@hdm-stuttgart.de>
2021-07-17 22:22:36 +0200

    add audio wait

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 
 from time import gmtime
 import time
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, audio
 
 from dateutil import relativedelta
 from datetime import date, datetime, timedelta, tzinfo
@@ -308,7 +308,6 @@ class CalendarManager(MycroftSkill):
 
             
             for i in range(number):
-                self.wait_while_speaking()
                 self.log.info(future_events[i].instance.vevent)
                 next_event = future_events[i].instance.vevent
                 starttime = self.get_time_string(next_event.dtstart.value) #TODO: add Duration
@@ -319,6 +318,7 @@ class CalendarManager(MycroftSkill):
                 end_date_string = f"{self.get_ordinal_number(next_event.dtstart.value.day)} of {next_event.dtstart.value.strftime('%B')}"
 
                 self.speak_dialog('yes.appointments', {'title': summary, 'startdate': start_date_string, 'starttime': starttime, 'enddate':end_date_string, 'endtime':endtime})
+                audio.wait_while_speaking()
 
 
 
